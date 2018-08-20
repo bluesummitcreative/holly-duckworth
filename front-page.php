4e5764efd0f6fca5cf8afaac2f3d3fb672dc5c4b
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-20 16:42:36 -0600

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,7 +1,16 @@
 <?php 
 	/*Template Name: Home*/
 	get_header();
-	get_template_part('inc/slider-home');
+	get_template_part('inc/slider-home'); ?>
+	<div class="container-fluid container-padding intro">
+		<div class="container">
+			<div class="row">
+				<?php echo do_shortcode('[gravityform id="1" title="false" description="false" ajax="true" tabindex="10"]'); ?>
+			</div>
+		</div>
+	</div>
+	
+<?php
 	get_template_part('inc/intro-home');
 	get_template_part('inc/funnels-home');
 	get_template_part('inc/site-wide-cta');
