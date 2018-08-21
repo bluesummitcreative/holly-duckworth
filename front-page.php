c3310fcbfec50840a262fdc73c6dc3eb13856901
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-20 19:04:27 -0600

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -17,7 +17,7 @@
 	<div class="container-fluid">
 		<div class="container">
 			<div class="row">
-				<div class="col-sm-12 text-center">
+				<div class="col-sm-12 text-center content-padding">
 					<h2>Benefits of Mindfulness Training</h2>
 				</div>
 				<div class="col-sm-4 text-center">
