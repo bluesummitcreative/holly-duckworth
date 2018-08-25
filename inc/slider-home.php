d392f6c69deda2dd948528500e8397ecf2f2fabe
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-25 13:51:30 -0600

    Update slider-home.php

--- inc/slider-home.php
+++ inc/slider-home.php
@@ -2,7 +2,9 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 col-md-6 offset-md-6">
-				<h1>Holly Duckworth: Keynote Speaker, Trainer and Mindfulness Coach<?php echo do_shortcode('[gravityform id="2" title="false" description="false" ajax="true" tabindex="10"]'); ?>
+				<h1>Holly Duckworth: Keynote Speaker, Trainer and Mindfulness Coach</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet lectus justo. Etiam sapien.</p>
+				<p><a href="#" class="btn btn-primary">Apply for Group Coaching</a></p>
 			</div>
 		</div>
 	</div>
