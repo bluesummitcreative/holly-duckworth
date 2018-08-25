d0c73a55d0a171f466591bfa0cfaefcc9420dc79
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-25 14:28:31 -0600

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -14,14 +14,14 @@
 			</div>
 		</div>
 	</div>
-	<div class="container-fluid container-padding">
+	<div class="container-fluid container-padding benefits">
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-12 text-center">
 					<h2>Benefits of Mindfulness Training</h2>
 				</div>
 				<div class="col-sm-4 text-center">
-					<i class="fas fa-heartbeat"></i>
+					<i class="fas fa-heartbeat fa-7x"></i>
 					<h4>Good for Heart</h4>
 					<p>Building mindfulness doesn't mean you have to sit still and watch your breath. The mind-calming effect of meditation can help reduce the risk of heart disease, according to Harvard research. Mindfulness is good for your head, teaching you that letting go of overwhelm is for your heart. The more mindful you become, the better decisions you will make, and the more you will achieve. You can find a mindfulness practice that suits your lifestyle.</p>
 				</div>
