d1f33c8c0b77f3cf6821bf13fb1af0d279f29011
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-25 13:21:50 -0600

    Update slider-home.php

--- inc/slider-home.php
+++ inc/slider-home.php
@@ -1,22 +1,12 @@
-<div class="slider">
-	 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <!--<ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>-->
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="http://via.placeholder.com/1920x800" alt="First slide">
-    </div>
-  </div>
-  <!--<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>-->
-</div>
+<div class="container-fluid" style="background:url(/wp-content/uploads/2018/08/slide1.jpg) no-repeat;">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				THIS WILL BE A BLANK COLUMN
+			</div>
+			<div class="col-sm-6">
+				FORM WILL GO HERE
+			</div>
+		</div>
+	</div>
 </div>
