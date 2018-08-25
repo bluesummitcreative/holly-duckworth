d41ee26184ebadd4a5a1ca87ef5e200402ef68a2
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-25 13:52:55 -0600

    Update slider-home.php

--- inc/slider-home.php
+++ inc/slider-home.php
@@ -1,4 +1,4 @@
-<div class="container-fluid" style="background:url(/wp-content/uploads/2018/08/slide1.jpg) no-repeat;background-position: center center;background-size: cover; padding: 200px;">
+<div class="slider container-fluid" style="background:url(/wp-content/uploads/2018/08/slide1.jpg) no-repeat;background-position: center center;background-size: cover; padding: 200px;">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-12 col-md-6 offset-md-6">
