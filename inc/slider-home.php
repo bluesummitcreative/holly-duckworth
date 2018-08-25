cef73ad2ca2cb6d1f33c77c1d07a64b89245c0b9
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-25 13:29:34 -0600

    Update slider-home.php

--- inc/slider-home.php
+++ inc/slider-home.php
@@ -1,7 +1,7 @@
 <div class="container-fluid" style="background:url(/wp-content/uploads/2018/08/slide1.jpg) no-repeat;background-position: center center;background-size: cover; padding: 200px;">
 	<div class="container">
 		<div class="row">
-			<div class="col-sm-6 sm-offset-6">
+			<div class="col-sm-6 offset-sm-6">
 				<?php echo do_shortcode('[gravityform id="2" title="false" description="false" ajax="true" tabindex="10"]'); ?>
 			</div>
 		</div>
