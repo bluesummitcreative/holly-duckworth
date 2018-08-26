4b69c596e6865cf69281539a0cb4c8c66a9f55e5
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-08-26 11:38:48 -0600

    Update slider-home.php

--- inc/slider-home.php
+++ inc/slider-home.php
@@ -2,9 +2,9 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6">
-				<h1>Holly Duckworth: Keynote Speaker, Trainer and Mindfulness Coach</h1>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sit amet lectus justo. Etiam sapien.</p>
-				<p><a href="#" class="btn btn-secondary">Invite Holly to Speak</a></p>
+				<h1>Holly Duckworth:<br /> Keynote Speaker and Stress Reduction Specialist</h1>
+				<p>Holly's known for her inclusive high energy, her ability to captivate audiences, and a vision of helping others reduce and eliminate stress from their lives.</p>
+				<p><a href="#" class="btn btn-secondary">Invite Holly to Speak</a> <a href="#" class="btn btn-secondary">Apply for Coaching</a></p>
 			</div>
 		</div>
 	</div>
