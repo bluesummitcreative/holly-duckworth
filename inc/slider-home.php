69e00ad7d6d6cbda56220dede3ba86d44b519c7a
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-09-02 11:56:37 -0600

    Update slider-home.php

--- inc/slider-home.php
+++ inc/slider-home.php
@@ -4,7 +4,7 @@
 			<div class="col-lg-6 offset-lg-0 text-lg-left offset-sm-3 text-sm-right">
 				<h1>Holly Duckworth:<br /> Keynote Speaker and Stress Reduction Specialist</h1>
 				<p>Holly's known for her inclusive high energy, her ability to captivate audiences, and a vision of helping others reduce and eliminate stress from their lives.</p>
-				<p><a href="#" class="btn btn-secondary btn-lg">Invite Holly to Speak</a></p>
+				<p><a href="/speaking/" class="btn btn-secondary btn-lg">Invite Holly to Speak</a></p>
 			</div>
 		</div>
 	</div>
