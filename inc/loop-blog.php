0641a84a84b66f6f2696898178f30568a79fde37
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-09-09 22:22:01 -0600

    Update loop-blog.php

--- inc/loop-blog.php
+++ inc/loop-blog.php
@@ -1,17 +1,13 @@
 <div class="col-sm-9 order-sm-2">
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<div class="row post">
-		<?php if(has_post_thumbnail()) { ?>
-			<div class="col-sm-3">
-				<a href="<?php the_permalink() ?>"><?php the_post_thumbnail(array('class' => 'img-fluid'));?></a>
-			</div>
-			<div class="col-sm-9">
-			<?php } else { ?>
-			<div class="col-sm-12">
-			<?php } ?>
-				<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-				<?php the_excerpt(); ?>
-			</div>
+		<div class="col-sm-12">
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<p style="font-size: 12px;"><em>By <span class="author"><?php the_author(); ?></span> - <span class="date"><?php the_time( get_option( 'date_format' ) ); ?></span><br />Categories: <span class="categories"><?php the_category(', '); ?></span></em></p>			<?php if(has_post_thumbnail()) { ?>
+			<a href="<?php the_permalink() ?>"><?php the_post_thumbnail(array('class' => 'img-fluid'));?></a>
+			<?php } else {} ?>
+			<?php the_excerpt(); ?>
+		</div>
 	</div>
 	
 	<?php wp_link_pages(); ?>
