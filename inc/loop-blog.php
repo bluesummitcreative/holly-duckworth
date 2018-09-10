57e6ab8416988854093ebc28a080ada64b6abaf6
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-09-09 22:24:45 -0600

    Update loop-blog.php

--- inc/loop-blog.php
+++ inc/loop-blog.php
@@ -6,7 +6,7 @@
 			<p style="font-size: 12px;"><em>By <span class="author"><?php the_author(); ?></span> - <span class="date"><?php the_time( get_option( 'date_format' ) ); ?></span><br />Categories: <span class="categories"><?php the_category(', '); ?></span></em></p>			<?php if(has_post_thumbnail()) { ?>
 			<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full',array('class' => 'img-fluid'));?></a>
 			<?php } else {} ?>
-			<p><?php the_excerpt(); ?></p>
+			<p style="margin-top:10px;"><?php the_excerpt(); ?></p>
 		</div>
 	</div>
 	
