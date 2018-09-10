844f9831d19c7d72e6c888d1893dfd9f4275ecdf
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-09-09 22:14:23 -0600

    Update page-header.php

--- inc/page-header.php
+++ inc/page-header.php
@@ -1,5 +1,5 @@
 <?php if ( is_home() ) { ?>
-<div class="container-fluid page-header" style="background:url(<?php if ( get_field( 'image', get_option('page_for_posts') ) ): the_field('image', get_option('page_for_posts')); else: header_image(); endif;?>) no-repeat;background-size:cover;background-position:center center;">
+<div class="container-fluid page-header" style="background:url(<?php if ( get_field( 'image', get_option('page_for_posts') ) ): the_field('image', get_option('page_for_posts')); else: the_field( 'default_page_header', 'option' ); endif;?>) no-repeat;background-size:cover;background-position:center center;">
 <?php } else { ?>
 <div class="container-fluid page-header" style="background:linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url(<?php if ( get_field( 'page_header_image' ) ): the_field('page_header_image'); else: the_field( 'default_page_header', 'option' ); endif;?>) no-repeat;background-size:cover;background-position:center center;">
 <?php } ?>
@@ -8,18 +8,14 @@
 			<div class="col-sm-12 text-center">
 				<h1><?php
 					if ( is_home() ) { 
-                            echo 'Blog';
-                        /*} elseif ( is_404() ) { 
-							echo '404 Not Found';*/
-						} elseif ( is_search() ) { 
-							echo $wp_query->found_posts . ' results found for "' . esc_html( get_search_query( false ) ) .'"';
-						
-						} else {
-							the_title();
-						}
+				   		echo 'The Mindfulness Blog';
+					} elseif ( is_search() ) { 
+						echo $wp_query->found_posts . ' results found for "' . esc_html( get_search_query( false ) ) .'"';
+					} else {
+						the_title();
+					}
 				?></h1>
 			</div>
 		</div>
 	</div>
 </div>
-
