a924cd1571636a8c4451fe961bd4c5ed07be70b8
bluesummitcreative <bluesummitcreative@users.noreply.github.com>
2018-10-15 10:46:25 -0600

    Create books.php

---
+++ books.php
@@ -0,0 +1,30 @@
+<?php
+	/*Template Name: Books*/
+	get_header();
+	get_template_part('inc/page-header');
+?>
+<div class="container-fluid container-padding">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<p style="text-align: center;"><a href="hhttps://www.amazon.com/Everyday-Mindfulness-Chaos-Crazy-World/dp/1732019827"><img class="img-fluid aligncenter wp-image-8982 size-full" src="https://hollyduckworth.com/wp-content/uploads/2018/10/EM3dCover.jpg" alt="" width="1627" height="1920" /></a></p>
+				<p style="text-align: center;">There are good days and there are bad days. And then, there is everyday. Our 24-hour day/7-day week/365-day year can be stress inducing and anxiety creating. Most of us are overwhelmed just getting through our days. This book teaches applied mindfulness off a yoga mat or meditation pillow.</p>
+				<p style="text-align: center;">Applied mindfulness consists of short two to five minute practices you can do within your "real life" to stay present in both good, and not so good, experiences in order to become your best self. Each day, you will set an intention and be inspired by quotes from historical and current public figures to assist you in mindful practices and wisdom. Whether you read once each day, or pick up this book from time to time, the experience will leave you calm and inspired.</p>
+				<p style="text-align: center;"><a class="btn btn-primary btn-lg" href="hhttps://www.amazon.com/Everyday-Mindfulness-Chaos-Crazy-World/dp/1732019827">Purchase Everyday Mindfulness: From Chaos To Calm In A Crazy World</a></p>
+			</div>
+			<div class="col-md-4">
+				<a href="https://www.amazon.com/dp/1732019819/ref=sr_1_fkmr1_3?ie=UTF8&amp;qid=1520294047&amp;sr=8-3-fkmr1&amp;keywords=atoz+guide+of+mindful+leadership" target="_blank" rel="noopener"><img class="wp-image-7571 size-full img-fluid aligncenter" src="https://hollyduckworth.com/wp-content/uploads/2018/03/mindful-leadership-cover-screengrab.gif" alt="" width="600" height="900" /></a>
+				<p style="text-align: center;">In a world where few can agree on anything, most can agree we live in a stressed-out world. The research points to the undeniable fact that organizations are struggling to find leaders because few people want to sell their life to the stress of leading organizations. Mindful Leadership: The A to Z Guide for Stress-free Leadership is a quick answer to this problem. <a href="https://hollyduckworth.com/wp-content/uploads/2018/09/MindfulLeadershipSample.pdf">Download a preview of Mindful Leadership here »</a></p>
+				<p style="text-align: center;"><a class="btn btn-primary btn-lg" href="https://www.amazon.com/dp/1732019819/ref=sr_1_fkmr1_3?ie=UTF8&amp;qid=1520294047&amp;sr=8-3-fkmr1&amp;keywords=atoz+guide+of+mindful+leadership">Purchase Mindful Leadership: The A to Z Guide For Stress-Free Leadership</a></p>
+			</div>
+			<div class="col-md-4">
+				<p style="text-align: center;"><a href="https://www.amazon.com/Ctrl-Alt-Believe-Association-Success/dp/1732019800/ref=sr_1_1?ie=UTF8&amp;qid=1527966171&amp;sr=8-1&amp;keywords=ctrl+alt+believe" target="_blank" rel="noopener"><img class="aligncenter size-large img-fluid" src="https://hollyduckworth.com/wp-content/uploads/2018/07/CABFrontCoverFinalSmall-683x1024.jpg" alt="" width="640" height="960" /></a></p>
+				<p style="text-align: center;">Countless challenges face associations today. As a leader, you choose how to respond to these challenges. Is your association stagnant and suffering, or can you create opportunities for higher levels of association achievement? Ctrl+Alt+Believe: Reboot Your Association For Success moves associations through a process to evaluate control, create alternate solutions and examine association beliefs.</p>
+				<p style="text-align: center;"><a class="btn btn-primary btn-lg" href="https://www.amazon.com/Ctrl-Alt-Believe-Association-Success/dp/1732019800/ref=sr_1_1?ie=UTF8&amp;qid=1527966171&amp;sr=8-1&amp;keywords=ctrl+alt+believe">Purchase Ctrl+Alt+Believe: Reboot Your Association For Success</a></p>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+	get_footer();
+?>
